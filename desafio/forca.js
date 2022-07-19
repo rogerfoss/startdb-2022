7d264f73a585b4db788a407a07bb3fa3856926f6
rogerfoss <rogerfsiqueira@gmail.com>
2022-07-19 14:41:57 -0300

    Estrutura da solução do desafio baseado nas regras

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,6 +1,39 @@
 class Forca {
+ 
+  vidas = 6; //regra 1.
+  estado = 'aguardando chute'; //regra 2.
+ 
+  chutar(letra) { 
+    
+    if (letra.length > 1) { //regra 3.
+      return;
+    } 
 
-  chutar(letra) { }
+    if (this.letrasChutadas.includes(letra)) { //regra 4.
+      return;
+    } 
+
+    if (this.palavra.includes(letra)) { 
+      this.letrasChutadas.push(letra); //regra 5.
+    } else {
+      this.vidas--; //regra 6.
+    }
+    
+    for (let i = 0; i < this.palavra.length; i++) { //regra 7.
+      if (this.palavra[i] === letra) {
+        this.palavra[i] = letra;    
+      }
+    }
+
+    if (this.vidas === 0) { //regra 8.
+      this.estado = 'perdeu';
+    } else if (this.palavra.join('') === this.buscarDadosDoJogo().palavra.join('')) { //regra 9.
+      this.estado = 'ganhou';
+      } else {
+      this.estado = 'aguardando chute';
+    }
+
+  }
 
   buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
